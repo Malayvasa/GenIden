db391374b8aae9e0923e5bfce0986506fc0215b3
malayvasa <malayvasa@gmail.com>
2020-11-16 18:12:05 +0530

    rm Easter Egg

--- js/Generate.js
+++ js/Generate.js
@@ -1,41 +1,27 @@
 function Generate(){ 
-    if(document.getElementById('name').value != ""){
-        var placeHolder = document.getElementById('name').value;
-        //Rick Roll
-        if(placeHolder.toLowerCase()=="rick"){
-            window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-        }
-        //Epic Sax
-        else if(placeHolder.toLowerCase()=="sex"){
-            window.location.replace("https://www.youtube.com/watch?v=6iFbuIpe68k");
-        }
-        else if(placeHolder.toLowerCase()=="george floyd"){
-            window.location.replace("https://blacklivesmatter.com/");
-        }
-        else{
-        name = placeHolder;
-        document.getElementById("feedback").innerHTML = "";
-        }
+  var input = document.getElementById('name').value;
+  if(input != ""){
+      name = input;
+      document.getElementById("feedback").innerHTML = "";
   } 
   else {
-        document.getElementById("feedback").style.color="red";
-        document.getElementById("feedback").innerHTML = "Invalid Name!";
+      document.getElementById("feedback").style.color="red";
+      document.getElementById("feedback").innerHTML = "Invalid Name!";
   }
-  
 }
-  function ToggleAnimate(){animate= -animate;}
-  
-  document.querySelector(".btn").addEventListener("click", Generate);
-  document.querySelectorAll(".btn")[1].addEventListener("click", function(){
-    animate = -animate;
-  });
-  document.querySelector("#name").addEventListener("keydown", function(e){
-    if(e.keyCode==13){
-      Generate();
-    }
-  })
 
- 
+function ToggleAnimate(){animate= -animate;}
+
+document.querySelector(".btn").addEventListener("click", Generate);
+document.querySelectorAll(".btn")[1].addEventListener("click", function(){
+  animate = -animate;
+});
+
+document.querySelector("#name").addEventListener("keydown", function(e){
+  if(e.keyCode==13){
+    Generate();
+  }
+})
 
 if ($(window).width() <= 812) {
   $('input').focus( function() {
